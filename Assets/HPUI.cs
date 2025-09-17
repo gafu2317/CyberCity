da79400680a9f2081ee37fd278933d4e1f91a4ea
asahi <yamamotoasahi046@gmail.com>
2025-09-17 14:14:38 +0900

    残基が１になった時に、画面が赤く点滅するようになりました。

--- Assets/HPUI.cs
+++ Assets/HPUI.cs
@@ -5,7 +5,8 @@ using System.Collections;
 public class HPUI : MonoBehaviour
 {
     [SerializeField] private Image[] barriers; // 右上のバリア画像を3つセット
-    [SerializeField] private int maxHP = 3;
+    [SerializeField] private Image warningPanel; // 赤い警告用のパネル
+    private int maxHP = 3;
 
     private int currentHP;
     private Coroutine redCoroutine; // 赤フェード用のコルーチン管理
@@ -13,6 +14,7 @@ public class HPUI : MonoBehaviour
     void Start()
     {
         currentHP = maxHP;
+        warningPanel.color = new Color(1, 0, 0, 0); // 完全透明
         UpdateUI();
     }
 
@@ -37,6 +39,10 @@ public class HPUI : MonoBehaviour
         // 残り1つのとき赤へ徐々にフェード
         if (currentHP == 1)
         {
+
+
+            StartCoroutine(WarningEffect()); // 警告開始
+
             if (redCoroutine == null) // まだ赤フェードが始まっていなければ
             {
                 redCoroutine = StartCoroutine(FadeToRed(barriers[0]));
@@ -80,5 +86,22 @@ public class HPUI : MonoBehaviour
         }
 
         img.color = end; // 最終的に赤固定
+    }
+
+    //警報がなってるみたいに画面を赤く点滅させる
+    private IEnumerator WarningEffect()
+    {
+        yield return new WaitForSeconds(1f); //２秒待つ
+
+        float speed = 2f; // 点滅スピード
+        while (currentHP == 1) // HPが1の間ずっと繰り返す
+        {
+            float t = (Mathf.Cos(Time.time * speed) + 1f) / 2f;
+            warningPanel.color = new Color(1, 0, 0, t * 0.5f);
+            yield return null;
+        }
+
+        // HPが1から変わったら終了
+        warningPanel.color = new Color(1, 0, 0, 0);
     }
 }
